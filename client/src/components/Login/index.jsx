35a5ae4e8abf9473ea8871a6a07b4b4acf033cc5
Yas Rossan <yaaseen.rossan95@gmail.com>
2023-05-30 23:53:50 +0100

    added form

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+    <h1>Login Page</h1>
+
+    <form>
+        <p>Username: </p>
+        <input type='text' aria-label='username input' placeholder='Enter Username'></input>
+        <p>Password: </p>
+        <input type='text' aria-label='password input' placeholder='Enter Password'></input><br/>
+        <input type='submit'></input>
+    </form>
+    </>
   )
 }
 
