ad6e129b9fbc2368ce020e44feb06b8e391eb672
Jack Moore <jack.d.moore@outlook.comssh-keygen>
2023-05-30 11:37:24 +0100

    main.jsx: Router updated

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+// import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import { BrowserRouter as Router } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <Router>
     <App />
-  </React.StrictMode>,
-)
+  </Router>
+);
+
