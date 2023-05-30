dbe41f667ed01e083ed80d4f5213f1054f6fe99a
Yas Rossan <yaaseen.rossan95@gmail.com>
2023-05-31 00:38:23 +0100

    added tracking for userinput in fields

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 const Login = () => {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  function handleUsername(e) {
+    // e.preventDefault()
+    setUsername(e.target.value)
+    
+  }
+
+  function handlePassword(e) {
+    setPassword(e.target.value)
+    
+  }
+
+  function gatherDetails(e){
+    e.preventDefault()
+    console.log(username)
+    console.log(password)
+  }
+
+
   return (
     <>
-    <h1>Login Page</h1>
+    
 
-    <form>
+    <form className='input-container'>
+        
         <p>Username: </p>
-        <input type='text' aria-label='username input' placeholder='Enter Username'></input>
+        <input type='text' aria-label='username input' placeholder='Enter Username' onChange={handleUsername} value={username}></input>
         <p>Password: </p>
-        <input type='text' aria-label='password input' placeholder='Enter Password'></input><br/>
-        <input type='submit'></input>
+        <input type='text' aria-label='password input' placeholder='Enter Password' onChange={handlePassword} value={password}></input><br/>
+        <input type='submit' onClick={gatherDetails}></input>
     </form>
     </>
   )
