632ca3166b997adf0c6406e509b31b0ed68e6b12
Tomislav Dinev <dinevtomislav@gmail.com>
2023-05-31 16:09:16 +0100

    finished the urgent page

--- client/src/pages/TodoPage/index.jsx
+++ client/src/pages/TodoPage/index.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
 
-const TodoPage = () => {
+const TodoPage = ({ tasks, onAddTask }) => {
   const [items, setItems] = useState([]);
   const [currentDate, setCurrentDate] = useState(new Date());
   const [points, setPoints] = useState(0);
-  const [showDoneTasks, setShowDoneTasks] = useState(true); 
+  const [showDoneTasks, setShowDoneTasks] = useState(true);
 
   const handleAddItem = (text) => {
-    const newItem = { id: Date.now(), text, done: false };
-    setItems([...items, newItem]);
-  };
+  const newItem = { id: Date.now(), text, done: false, dueDate: currentDate.toISOString() };
+  setItems([...items, newItem]);
+  onAddTask(text, currentDate.toISOString());
+};
+
 
   const handleToggleDone = (id) => {
     const updatedItems = items.map((item) => {
@@ -18,7 +20,7 @@ const TodoPage = () => {
         if (updatedItem.done) {
           setPoints((prevPoints) => prevPoints + 100);
         } else {
-          setPoints((prevPoints) => prevPoints - 100); 
+          setPoints((prevPoints) => prevPoints - 100);
         }
         return updatedItem;
       }
@@ -39,7 +41,7 @@ const TodoPage = () => {
 
   const handleDeleteItem = (id, done) => {
     if (done) {
-      setPoints((prevPoints) => prevPoints - 100); 
+      setPoints((prevPoints) => prevPoints - 100);
     }
     const updatedItems = items.filter((item) => item.id !== id);
     setItems(updatedItems);
@@ -89,7 +91,7 @@ const TodoPage = () => {
         {showDoneTasks ? 'Show Todos' : 'Show Done'}
       </button>
 
-      <h3>{showDoneTasks ? 'Done Tasks' : 'Todo Tasks'}</h3> {}
+      <h3>{showDoneTasks ? 'Done Tasks' : 'Todo Tasks'}</h3>
       <TodoList
         items={handleFilterTasks()}
         onToggleDone={handleToggleDone}
@@ -106,7 +108,8 @@ const TodoForm = ({ onAddItem }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (text.trim()) {
-      onAddItem(text.trim());
+      const dueDate = new Date();
+      onAddItem(text.trim(), dueDate);
       setText('');
     }
   };
@@ -133,7 +136,7 @@ const TodoList = ({ items, onToggleDone, onEditItem, onDeleteItem }) => {
           item={item}
           onToggleDone={onToggleDone}
           onEditItem={onEditItem}
-          onDeleteItem={() => onDeleteItem(item.id, item.done)} 
+          onDeleteItem={onDeleteItem}
         />
       ))}
     </ul>
@@ -153,15 +156,63 @@ const TodoItem = ({ item, onToggleDone, onEditItem, onDeleteItem }) => {
   };
 
   const handleDelete = () => {
-    onDeleteItem(item.id, item.done); 
+    onDeleteItem(item.id, item.done);
   };
+
+  const getDaysRemaining = (dueDate) => {
+    const today = new Date();
+    const due = new Date(dueDate);
+    const timeDiff = due.getTime() - today.getTime();
+    const daysRemaining = Math.ceil(timeDiff / (1000 * 3600 * 24));
+    return daysRemaining >= 0 ? daysRemaining : 0;
+  };
+
+  const getTaskColor = (daysRemaining) => {
+    if (daysRemaining >= 7) {
+      return 'yellow';
+    } else if (daysRemaining >= 3 && daysRemaining <= 6) {
+      return 'orange';
+    } else {
+      return 'red';
+    }
+  };
+
+  const daysRemaining = getDaysRemaining(item.dueDate);
+
+  const renderDaysRemaining = () => {
+    if (item.done) {
+      return <span className="completed">Completed</span>;
+    } else {
+      const currentDate = new Date();
+      const dueDate = new Date(item.dueDate);
+      const timeDifference = dueDate.getTime() - currentDate.getTime();
+      const daysRemaining = Math.ceil(timeDifference / (1000 * 3600 * 24));
+  
+      let colorClass = '';
+  
+      if (daysRemaining >= 7) {
+        colorClass = 'yellow';
+      } else if (daysRemaining >= 3 && daysRemaining <= 6) {
+        colorClass = 'orange';
+      } else {
+        colorClass = 'red';
+      }
+  
+      return <span className={colorClass}>{daysRemaining} day(s) remaining</span>;
+    }
+  };
+
+  const taskColor = getTaskColor(daysRemaining);
 
   return (
     <li>
-      <input type="checkbox" checked={item.done} onChange={handleToggle} />
-      <span className={item.done ? 'done' : ''}>{item.text}</span>
-      <button onClick={handleEdit}>Edit</button>
-      <button onClick={handleDelete}>Delete</button>
+      <div>
+        <input type="checkbox" checked={item.done} onChange={handleToggle} />
+        <span className={item.done ? 'done' : ''}>{item.text}</span>
+        <span className={taskColor}>{daysRemaining} days left</span>
+        <button onClick={handleEdit}>Edit</button>
+        <button onClick={handleDelete}>Delete</button>
+      </div>
     </li>
   );
 };
