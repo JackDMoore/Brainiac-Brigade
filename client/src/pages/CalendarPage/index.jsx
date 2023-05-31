913e3e2cf6f2dea65710da89b3970cb275391938
Jack Moore <jack.d.moore@outlook.comssh-keygen>
2023-05-31 11:22:15 +0100

    calendar page now links to todo page

--- client/src/pages/CalendarPage/index.jsx
+++ client/src/pages/CalendarPage/index.jsx
@@ -1,17 +1,23 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Calendar from "react-calendar";
+import React from "react";
 // import "./App.css";
 
 const CalendarPage = () => {
   const [date, setDate] = useState(new Date());
+  let navigate = useNavigate();
+
+  const handleNavigation = () => {
+    navigate("/todo");
+  };
 
   return (
     <div className="app">
       <h1 className="header">Calendar</h1>
       <div className="calendar-container">
-        <Calendar onChange={setDate} value={date} />
+        <Calendar onChange={handleNavigation} />
       </div>
-      <div className="text-center">Selected date: {date.toDateString()}</div>
     </div>
   );
 };
