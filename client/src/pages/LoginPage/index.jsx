87e997d3564baf7a5f7193c724a6f81988b28317
Yas Rossan <yaaseen.rossan95@gmail.com>
2023-05-31 01:40:09 +0100

    added callback function

--- client/src/pages/LoginPage/index.jsx
+++ client/src/pages/LoginPage/index.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import { Login } from '../../components'
 
 const LoginPage = () => {
+  
+
+  function CallBack(user,pass){
+    console.log("user: ",user)
+    console.log("pass: ",pass)
+    
+  }
   return (
     <>
-        <Login/>
+        <h1>Login Page</h1>
+        <Login callback={CallBack}/>
     </>
   )
 }
