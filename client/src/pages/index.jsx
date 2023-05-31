f10e67771c99ad355f48fc9c9edff06fb5890e24
Leahjthom <leahjthom@gmail.com>
2023-05-31 12:11:12 +0100

    todo

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,7 +1,7 @@
 export { default as HomePage } from './Homepage';
 export { default as LoginPage } from './LoginPage';
 export { default as LandingPage } from './LandingPage';
-// export { default as TodoPage } from './TodoPage';
+export { default as TodoPage } from './TodoPage';
 export { default as NotFoundPage } from './NotFoundPage';
 export { default as CalendarPage } from "./CalendarPage" 
 export { default as RegisterPage} from './RegisterPage'
