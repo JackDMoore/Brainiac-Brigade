ddad87b04f5f9644048885107c8a209895c3174b
Diego Ramos <diegorramos84@gmail.com>
2023-06-01 10:27:49 +0100

    change index to only return events form logged user

--- api/models/Event.js
+++ api/models/Event.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const eventSchema = new mongoose.Schema({
   text: { type: String, required: true},
-  done: {type: String, default: ''},
+  done: {type: Boolean, default: false},
   start : {
     type: Date,
     // required: [true, 'Please insert the start of your event']
