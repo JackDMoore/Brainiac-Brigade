cd2ca50d81f68f4dd0dfb5426d591f919794c785
Jack Moore <jack.d.moore@outlook.comssh-keygen>
2023-06-01 10:15:18 +0100

    homepage fourth test working

--- client/src/pages/Homepage/HomePage.test.jsx
+++ client/src/pages/Homepage/HomePage.test.jsx
@@ -9,7 +9,7 @@ import { screen, render, cleanup } from "@testing-library/react";
 import matchers from "@testing-library/jest-dom/matchers";
 expect.extend(matchers);
 
-describe("Calendar Page", () => {
+describe("Home Page", () => {
   beforeEach(() => {
     render(
       <Router>
